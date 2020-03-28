bb6ef4d3717ae647f31280c803f21a7f0dc177a3
utzcoz <utzcoz@outlook.com>
2020-03-28 23:28:06 +0800

    Add equals, hashcode and toString tests for Rect
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- src/test/kotlin/com/utzcoz/parser/dumpsys/RectTest.kt
+++ src/test/kotlin/com/utzcoz/parser/dumpsys/RectTest.kt
@@ -1,6 +1,7 @@
 package com.utzcoz.parser.dumpsys
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
 
 class RectTest {
@@ -18,5 +19,27 @@ class RectTest {
     fun testParseRectWithInvalidString() {
         val input = "100, 200, 300"
         Rect.parseRect(input)
+    }
+
+    @Test
+    fun testRectEquals() {
+        val rect1 = Rect(100, 200, 300, 400)
+        val rect2 = Rect(100, 200, 300, 400)
+        assertEquals(rect1, rect2)
+        val rect3 = Rect(100, 200, 300, 500)
+        assertNotEquals(rect1, rect3)
+    }
+
+    @Test
+    fun testRectHashCode() {
+        val rect1 = Rect(100, 200, 300, 400)
+        val rect2 = Rect(100, 200, 300, 400)
+        assertEquals(rect1.hashCode(), rect2.hashCode())
+    }
+
+    @Test
+    fun testRectToString() {
+        val rect1 = Rect(100, 200, 300, 400)
+        assertEquals("Rect(100, 200, 300, 400)", rect1.toString())
     }
 }
