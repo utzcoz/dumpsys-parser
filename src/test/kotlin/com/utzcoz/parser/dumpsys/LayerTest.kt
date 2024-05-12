a595ab2aa52ad22bf0da10dd4989b6e4dc9349ef
utzcoz <utzcoz@outlook.com>
2024-05-12 11:44:55 +0800

    Migrate junit4 to junit5
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- src/test/kotlin/com/utzcoz/parser/dumpsys/LayerTest.kt
+++ src/test/kotlin/com/utzcoz/parser/dumpsys/LayerTest.kt
@@ -1,7 +1,7 @@
 package com.utzcoz.parser.dumpsys
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
