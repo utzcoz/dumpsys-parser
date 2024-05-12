6dcda265c543051b35ba9fcdcc74595f44adc3a6
utzcoz <utzcoz@outlook.com>
2024-05-12 12:29:25 +0800

    Use Spotless to format kts files
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,62 +1,68 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-	id("com.github.johnrengelman.shadow") version "8.1.1"
-	id("java")
-	id("org.jetbrains.kotlin.jvm") version "1.9.24"
-	id("com.diffplug.spotless") version "6.25.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.24"
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "org.utzcoz.parser.dumpsys"
 version = "0.2"
 
 repositories {
-	mavenCentral()
-	gradlePluginPortal()
+    mavenCentral()
+    gradlePluginPortal()
 }
 
 spotless {
-	kotlin {
-		target("**/*.kt")
-		ktlint()
-	}
-	format("misc") {
-		// define the files to apply `misc` to
-		target("*.gradle", "*.md", ".gitignore")
+    kotlin {
+        target("**/*.kt")
+        ktlint()
+    }
 
-		// define the steps to apply to those files
-		trimTrailingWhitespace()
-		indentWithTabs() // or spaces. Takes an integer argument if you don't like 4
-		endWithNewline()
-	}
+    kotlinGradle {
+        target("*.kts", "**/*.kts")
+        ktlint()
+    }
+
+    format("misc") {
+        // define the files to apply `misc` to
+        target("*.gradle", "*.md", ".gitignore")
+
+        // define the steps to apply to those files
+        trimTrailingWhitespace()
+        indentWithTabs() // or spaces. Takes an integer argument if you don't like 4
+        endWithNewline()
+    }
 }
 
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 java {
-	toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
 }
 
 kotlin {
-	compilerOptions {
-		jvmTarget.set(JvmTarget.JVM_1_8)
-	}
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_1_8)
+    }
 }
 
 tasks.test {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.jar {
-	manifest {
-		attributes(
-				"Implementation-Title" to project.name,
-				"Implementation-Version" to project.version,
-				"Main-Class" to "com.utzcoz.parser.dumpsys.DumpsysParser"
-		)
-	}
+    manifest {
+        attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version,
+            "Main-Class" to "com.utzcoz.parser.dumpsys.DumpsysParser",
+        )
+    }
 }
